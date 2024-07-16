e7637934c2482b2a04cc807e700d7ca67588fa53
LukeBanta <lukebanta2008@gmail.com>
2024-07-16 14:34:44 -0400

    h

--- Adrenaline Shift/Assets/Scripts/CamFollow.cs
+++ Adrenaline Shift/Assets/Scripts/CamFollow.cs
@@ -6,16 +6,16 @@ public class CamFollow : MonoBehaviour
 {
     public GameObject player;
     public Vector3 offset = new Vector3(0, 5, 6);
-    private float xAngle, zAngle;
-    // Start is called before the first frame update
+
+
     void Start()
     {
 
     }
-
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
+
+
 
         transform.position = player.transform.position + offset;
 
