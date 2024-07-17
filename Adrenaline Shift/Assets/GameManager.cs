91863db4e00f20c0981b6968ed10ad05772500d2
LukeBanta <lukebanta2008@gmail.com>
2024-07-17 14:28:22 -0400

    h

--- Adrenaline Shift/Assets/GameManager.cs
+++ Adrenaline Shift/Assets/GameManager.cs
@@ -5,20 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    bool CarSelectLoaded = false;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
 
+    }
     // Update is called once per frame
     void Update()
     {
+        if (CarSelectLoaded == false)
+        {
+            // Loads the second Scene
+            
+        }
         
     }
 
-    void CarSelect()
+    public void CarSelect()
     {
-        SceneManager.SetActiveScene(SceneManager.GetSceneByName("CarSelect"));
+        //SceneManager.SetActiveScene(SceneManager.GetSceneByName("CarSelect"));
+        SceneManager.LoadScene("CarSelect");
+        CarSelectLoaded = true;
     }
 }
