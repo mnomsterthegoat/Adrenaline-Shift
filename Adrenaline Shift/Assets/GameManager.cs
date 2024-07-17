16a668816360b7dc09c3b2bb00ec25c7842c9e01
LukeBanta <lukebanta2008@gmail.com>
2024-07-17 14:04:15 -0400

    j

--- Adrenaline Shift/Assets/GameManager.cs
+++ Adrenaline Shift/Assets/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -14,5 +15,10 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void CarSelect()
+    {
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName("CarSelect"));
     }
 }
