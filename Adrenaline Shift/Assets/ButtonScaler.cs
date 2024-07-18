44bd625cf20058b2d4f9ee5aa6d5ee0de377aa54
Ericcccccccccccccccccccc <ys6731@nyu.edu>
2024-07-18 10:37:17 -0400

    button

---
+++ Adrenaline Shift/Assets/ButtonScaler.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.EventSystems;
+
+public class ButtonScaler: MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public TMP_Text buttonText;
+    public float hoverScale = 1.2f;
+
+    private Vector3 originalScale;
+
+    void Start()
+    {
+        if (buttonText == null)
+        {
+            buttonText = GetComponentInChildren<TMP_Text>();
+        }
+
+        originalScale = buttonText.transform.localScale;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        buttonText.transform.localScale = originalScale * hoverScale;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        buttonText.transform.localScale = originalScale;
+    }
+}
